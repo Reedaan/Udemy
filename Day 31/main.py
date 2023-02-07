0f1ad41e957b47f6cfb54c392e86e995d8c26326
Reedaan <119610449+Reedaan@users.noreply.github.com>
2023-02-07 11:04:39 +0100

    Update main.py

--- Day 31/main.py
+++ Day 31/main.py
@@ -10,36 +10,34 @@ try:
         r"D:\Python\Python Bootcamp\Day 31\data\words_to_learn.csv")
     data_dict = data.to_dict(orient="records")
 except FileNotFoundError:
-    data = pandas.read_csv(r"D:\Python\Python Bootcamp\Day 31\data\french_words.csv")
+    data = pandas.read_csv(
+        r"D:\Python\Python Bootcamp\Day 31\data\french_words.csv")
     data_dict = data.to_dict(orient="records")
 
-known = {}
 current_card = {}
 
-def add_to_known():
-    global current_card, flip_timer
-    window.after_cancel(flip_timer)
-    current_card = random.choice(data_dict)
-    canvas.itemconfig(create_title, text="French", fill="black")
-    canvas.itemconfig(create_description, text=current_card["French"], fill="black")
-    canvas.itemconfig(main_photo, image=card_front_png)
-    flip_timer = window.after(3000, func=flip_card)
-    if current_card in data_dict and not known:
-        data_dict.remove(current_card)
-        known[current_card["French"]]
-        print(known)
-
-
 def generate_new():
-    
+
     global current_card, flip_timer
     window.after_cancel(flip_timer)
     current_card = random.choice(data_dict)
     canvas.itemconfig(create_title, text="French", fill="black")
-    canvas.itemconfig(create_description, text=current_card["French"], fill="black")
+    canvas.itemconfig(create_description,
+                      text=current_card["French"], fill="black")
     canvas.itemconfig(main_photo, image=card_front_png)
     flip_timer = window.after(3000, func=flip_card)
-    
+
+
+def add_to_known():
+    if current_card in data_dict:
+        data_dict.remove(current_card)
+
+    df = pandas.DataFrame(data_dict)
+    df.to_csv(
+        r"D:\Python\Python Bootcamp\Day 31\data\words_to_learn.csv", index=False)
+
+    generate_new()
+
 
 # ---------------------------- GUI ------------------------------- #
 # Window config
@@ -47,8 +45,9 @@ def generate_new():
 def flip_card():
     canvas.itemconfig(main_photo, image=card_back_png)
     canvas.itemconfig(create_title, text="English", fill="white")
-    canvas.itemconfig(create_description, text=current_card["English"], fill="white")
-    
+    canvas.itemconfig(create_description,
+                      text=current_card["English"], fill="white")
+
 
 window = Tk()
 window.title("Flash card Project")
@@ -63,7 +62,6 @@ card_front_png = PhotoImage(
 card_back_png = PhotoImage(
     file=r"D:\Python\Python Bootcamp\Day 31\images\card_back.png")
 
-
 canvas = Canvas(width=800, height=526,
                 bg=BACKGROUND_COLOR, highlightthickness=0)
 main_photo = canvas.create_image(400, 263, image=card_front_png)
@@ -72,7 +70,6 @@ create_title = canvas.create_text(
 create_description = canvas.create_text(
     400, 263, text="", font=(FONT_NAME, 60, "bold"))
 canvas.grid(row=0, column=0, columnspan=2)
-
 
 # Buttons
 x_image = PhotoImage(file=r"D:\Python\Python Bootcamp\Day 31\images\wrong.png")
